336cab6136e5fff66387c45aa0d4aae1ce2abf3b
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-11-12 18:37:04 +0700

    Create SegiTiga.java

---
+++ SegiTiga.java
@@ -0,0 +1,17 @@
+paket  Nusput . P6 . Bangun_Datar ;
+
+public  class  SegiTiga  extends  BangunDatar {
+
+     pelampung  pribadi sayangnya ;
+     kendaraan  pribadi tinggi ;
+
+    publik  SegiTiga ( float  alas , float  tinggi ){
+        ini . sayang = sayang ;
+        ini . tinggi = tinggi ;
+    }
+
+    @ Ganti
+    apung  getLuas (){
+        return ( float ) 0.5 * alas * tinggi ;
+    }
+}
