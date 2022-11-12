410c9779748c0c6b2d4cb5bb3c5609c810784180
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-11-12 17:27:30 +0700

    Create BangunDatar.java

---
+++ BangunDatar.java
@@ -0,0 +1,15 @@
+paket  Nusput . P6 . Bangun_Datar ;
+
+ kelas abstrak  publik BangunDatar { 
+    Warna  tali ;
+
+    String  getName (){
+        kembali  warna ;
+    }
+
+    batal  setWarna ( String  warna ){
+        ini . warna = warna ;
+    }
+    
+    abstrak  float  getLuas ();
+}
