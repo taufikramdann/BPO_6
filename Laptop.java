098bd2273211e2e649ad474af70cca897c3ca8a7
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-11-12 18:40:34 +0700

    Create Laptop.java

---
+++ Laptop.java
@@ -0,0 +1,11 @@
+paket  Nusput . P6 . Laptopku ;
+
+ Laptop antarmuka  publik {
+    int  MAX_VOL = 100 ;
+    int  MIN_VOL = 0 ;
+
+    batalkan  powerOn ();
+    batal  mematikan daya ();
+    batal  volumeUp ();
+    batalkan  volumeBawah ();
+}
