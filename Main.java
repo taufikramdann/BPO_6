7d5d731c428dea9274d39bd21f2accb27e0df05b
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-11-12 18:34:56 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+paket  Nusput . P6 . Bangun_Datar ;
+
+ kelas  publik Utama {
+    public  static  void  main ( String [] args ) {
+         Segitiga BangunDatar = new  SegiTiga ( 12 , 20 );
+         Lingkaran BangunDatar = Lingkaran baru  ( 60 );
+
+        Sistem . keluar . println ( "Luas dari bangun datar segitiga : " + segitiga .getLuas ( ));
+        Sistem . keluar . println ( "Luas dari bangun datar luangkaran : " + lingkaran . getLuas ());
+    }
+}
